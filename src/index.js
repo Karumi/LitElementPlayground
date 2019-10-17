7dcb7cbf3faca3bc76bd0d5e6ce9151ed6206a01
tonilopezmr <tonilopezmr@gmail.com>
2019-10-17 16:39:39 +0200

    Fix cypress window.Cypress is not defined sometimes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './views/header-view.js';
 import { Router } from '@vaadin/router';
 import { store } from './redux/store.js';
 
+window.store = store;
+
 window.addEventListener('load', () => {
   initRouter();
   registerServiceWorker();
@@ -43,10 +45,4 @@ async function registerServiceWorker() {
   } else {
     console.log('Your browser does not support ServiceWorker.');
   }
-}
-
-// expose store when run in Cypress
-if (window.Cypress) {
-  console.log("Set Cypress store");
-  window.store = store;
 }
